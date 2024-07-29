d2fe7abc97655f98c9117654d558cf9c86c1a926
Anupam Raj <anupam.r2301@gmail.com>
2024-07-29 23:44:08 +0530

    version-2.0

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-const mobilemenubutton = document.querySelector("#mobile-menu-button");
-const mobilemenu = document.querySelector("#mobile-menu")
+document.addEventListener("DOMContentLoaded", function () {
 
-mobilemenubutton.addEventListener("click",()=>{
-    mobilemenu.classList.toggle("hidden")
-})
+
+
+    // menu bar logic
+
+    const mobilemenubutton = document.querySelector("#mobile-menu-button");
+
+    mobilemenubutton.addEventListener("click", () => {
+
+        const mobilemenu = document.querySelector("#mobile-menu")
+        mobilemenu.classList.toggle("hidden")
+    })
+
+
+
+
+    // scroll top logic
+
+
+    const movetopbtn = document.querySelector("#move-to-top");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 100) {
+            movetopbtn.classList.remove("hidden");
+            movetopbtn.classList.add("block");
+        }
+        else {
+            movetopbtn.classList.remove("block");
+            movetopbtn.classList.add("hidden")
+        }
+    });
+
+    movetopbtn.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    })
+
+
+});
